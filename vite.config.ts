b81466035aaf6b8d9ff0cb5533a4108a64738569
poppyseedcake <w.makowiec@gmail.com>
2026-07-06 13:43:59 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit()],
+  ssr: {
+    external: ['@opentelemetry/api']
+  },
   test: {
     include: ['src/**/*.test.ts']
   }
